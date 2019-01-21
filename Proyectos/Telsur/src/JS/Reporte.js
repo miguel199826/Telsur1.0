ed0140138794e18b5936290f359c05f96363044c
miguel199826 <miguel199826@gmail.com>
2019-01-20 19:11:29 -0500

    TelsurProyect

---
+++ Proyectos/Telsur/src/JS/Reporte.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('select').formSelect();
+});
+
+$(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'https://placehold.it/250x250'
+        },
+    });
+});
+$(document).ready(function(){
+    $('input.autocomplete2').autocomplete({
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'https://placehold.it/250x250'
+        },
+    });
+});
+
